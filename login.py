4aa09ad670da8e6baa2ac14d7be6818fa9bfb16e
Tetra <giggabyte6@gmail.com>
2023-05-11 12:09:44 -0500

    Failed to make multiline messages a thing, added ws and API endpoint options to login tho

--- login.py
+++ login.py
@@ -4,14 +4,19 @@ import json
 import yaml
 
 import globals
-
 class LoginForm(npyscreen.ActionForm):
     def create(self):
         self.name = 'Login'
         self.email=self.add(npyscreen.TitleText, name='Email')
         self.password=self.add(npyscreen.TitlePassword, name='Password')
+        self.add(npyscreen.FixedText, value='Advanced options')
+        self.apiEnd=self.add(npyscreen.TitleText, name='API endpoint')
+        self.wsEnd=self.add(npyscreen.FixedText, name='WS endpoint')
         self.edit()
     def on_ok(self):
+        if self.apiEnd.value and self.wsEnd.value:
+            globals.apiEndpoint=self.apiEnd.value
+            globals.wsEndpoint=self.wsEnd.value
         if self.email.value and self.password.value:
             response = requests.post(f'{globals.apiEndpoint}/auth/session/login', data=json.dumps({'email': self.email.value, 'password': self.password.value}))
             if response.status_code != 200:
